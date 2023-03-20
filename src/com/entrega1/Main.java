2fcee3d261e359f0e5fefca5af266fe2c8044315
Brunasso <brunotomasso1065@hotmail.com>
2023-03-19 21:31:53 -0300

    Create project

---
+++ src/com/entrega1/Main.java
@@ -0,0 +1,20 @@
+package com.entrega1;
+
+public class Main {
+    public static void main(String[] args) {
+        byte num1 = 1;
+        short num2 = 2;
+        int num3 = 3;
+        long num4 = 4;
+
+        float num5 = 5.5f;
+        double num6 = 6.6;
+
+        char letra = 'a';
+
+        boolean v = true;
+
+        System.out.println("num1: " + num1 + "\nnum2: " + num2 + "\nnum3: " + num3 + "\nnum4: " + num4
+                + "\nnum5: " + num5 + "\nnum6: " + num6 + "\nletra: " + letra + "\nv: " + v);
+    }
+}
